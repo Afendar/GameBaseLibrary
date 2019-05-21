162fe826094c69d1ad860e6ca14edb1568c88657
Afendar <mickael.comesse@gmail.com>
2019-05-21 21:54:06 +0200

    [ADD] Assets loading system

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "assets/TexturesLoader.h"
+
+int main(int argc, char *argv[])
+{
+	GDLib::Assets::TexturesLoader* texturesLoader = new GDLib::Assets::TexturesLoader();
+
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
